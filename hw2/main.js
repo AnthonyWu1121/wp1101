8d5fab8fcdeef9feb94d50c2cec8ec25953d9c58
unknown <anthony.hc.wu@gmail.com>
2021-10-15 01:06:08 +0800

    2021/10/15 01:06

--- hw2/main.js
+++ hw2/main.js
@@ -38,13 +38,24 @@ let album3 = [
 album3["Name"] = "album3";
 let album4 = []
 album4["Name"] = "album4";
+let album5 = []
+album5["Name"] = "album5";
+let album6 = []
+album6["Name"] = "album6";
+let album7 = []
+album7["Name"] = "album7";
+let album8 = []
+album8["Name"] = "album8";
+let album9 = []
+album9["Name"] = "album9";
+let album10 = []
+album10["Name"] = "album10";
 
 let displayImg = document.getElementById("largerPic");
 
 let picArray = [];
 function setImg(temp, id){
     displayImg.src = temp;
-    
     if(picArray.length !== 0){
         let prevImg = document.getElementById(picArray[0]);
         prevImg.classList.remove("selectedPic");
@@ -53,6 +64,7 @@ function setImg(temp, id){
     let selected = document.getElementById(id);
     picArray.push(id);
     selected.classList.add("selectedPic");
+    show_num();
 }
 
 function clearImg(){
@@ -74,6 +86,8 @@ function setAlbum(album, id){
         selected.classList.add("selectedAlbum");
         deleteChild();
         clearImg();
+        let picNum = document.getElementById("picNum");
+        picNum.textContent = "0/0";
     }
     else{
         deleteChild();
@@ -90,6 +104,7 @@ function setAlbum(album, id){
         let selected = document.getElementById(id);
         albArray.push(id);
         selected.classList.add("selectedAlbum");
+        show_num();
     }
 }
 
@@ -115,26 +130,222 @@ function deleteChild() {
 function add_new_img(){
     let newURL = prompt("What is the path of the new photo?")
 
-    if(album1.Name === albArray[0]){
+    if(newURL === null){
+        return;
+    }else if(album1.Name === albArray[0]){
         add_img(newURL, album1.length);
         setImg(newURL, "smallerPic" + album1.length);
         album1.push(newURL);
-    }
-    else if(album2.Name === albArray[0]){
+    }else if(album2.Name === albArray[0]){
         add_img(newURL, album2.length);
         setImg(newURL, "smallerPic" + album2.length);
         album2.push(newURL);
-    }
-    else if(album3.Name === albArray[0]){
+    }else if(album3.Name === albArray[0]){
         add_img(newURL, album3.length);
         setImg(newURL, "smallerPic" + album3.length);
         album3.push(newURL);
-    }
-    else if(album4.Name === albArray[0]){
+    }else if(album4.Name === albArray[0]){
         add_img(newURL, album4.length);
         setImg(newURL, "smallerPic" + album4.length);
         album4.push(newURL);
+    }else if(album5.Name === albArray[0]){
+        add_img(newURL, album5.length);
+        setImg(newURL, "smallerPic" + album5.length);
+        album5.push(newURL);
+    }else if(album6.Name === albArray[0]){
+        add_img(newURL, album6.length);
+        setImg(newURL, "smallerPic" + album6.length);
+        album6.push(newURL);
+    }else if(album7.Name === albArray[0]){
+        add_img(newURL, album7.length);
+        setImg(newURL, "smallerPic" + album7.length);
+        album7.push(newURL);
+    }else if(album8.Name === albArray[0]){
+        add_img(newURL, album8.length);
+        setImg(newURL, "smallerPic" + album8.length);
+        album8.push(newURL);
+    }else if(album9.Name === albArray[0]){
+        add_img(newURL, album9.length);
+        setImg(newURL, "smallerPic" + album9.length);
+        album9.push(newURL);
+    }else if(album10.Name === albArray[0]){
+        add_img(newURL, album10.length);
+        setImg(newURL, "smallerPic" + album10.length);
+        album10.push(newURL);
+    }
+    show_num();
+}
+
+function delete_img(){
+    let target = picArray.shift();
+    let numInstr = target.slice(10);
+    let numInInt = parseInt(numInstr);
+    if(album1.Name === albArray[0]){
+        album1.splice(numInInt,1);
+        setAlbum(album1,"album1");
+    }else if(album2.Name === albArray[0]){
+        album2.splice(numInInt,1);
+        setAlbum(album2,"album2");
+    }else if(album3.Name === albArray[0]){
+        album3.splice(numInInt,1);
+        setAlbum(album3,"album3");
+    }else if(album4.Name === albArray[0]){
+        album4.splice(numInInt,1);
+        setAlbum(album4,"album4");
+    }else if(album5.Name === albArray[0]){
+        album5.splice(numInInt,1);
+        setAlbum(album5,"album5");
+    }else if(album6.Name === albArray[0]){
+        album6.splice(numInInt,1);
+        setAlbum(album6,"album6");
+    }else if(album7.Name === albArray[0]){
+        album7.splice(numInInt,1);
+        setAlbum(album7,"album7");
+    }else if(album8.Name === albArray[0]){
+        album8.splice(numInInt,1);
+        setAlbum(album8,"album8");
+    }else if(album9.Name === albArray[0]){
+        album9.splice(numInInt,1);
+        setAlbum(album9,"album9");
+    }else if(album10.Name === albArray[0]){
+        album10.splice(numInInt,1);
+        setAlbum(album10,"album10");
     }
 }
 
-setAlbum(album1, "album1");
+function  add_new_album(){
+    var x = document.getElementById("album2");
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x = document.getElementById("album3");
+        if(x.style.display === "none"){
+            x.style.display = "block";
+        }else{
+            x = document.getElementById("album4");
+            if(x.style.display === "none"){
+                x.style.display = "block";
+            }else{
+                x = document.getElementById("album5");
+                if(x.style.display === "none"){
+                    x.style.display = "block";
+                }else{
+                    x = document.getElementById("album6");
+                    if(x.style.display === "none"){
+                        x.style.display = "block";
+                    }else{
+                        x = document.getElementById("album7");
+                        if(x.style.display === "none"){
+                            x.style.display = "block";
+                        }else{
+                            x = document.getElementById("album8");
+                            if(x.style.display === "none"){
+                                x.style.display = "block";
+                            }else{
+                                x = document.getElementById("album9");
+                                if(x.style.display === "none"){
+                                    x.style.display = "block";
+                                }else{
+                                    x = document.getElementById("album10");
+                                    if(x.style.display === "none"){
+                                        x.style.display = "block";
+                                    }else{
+                                        alert("Reached Maximun 10 Albums!");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function hide_album(){
+    let albumToDelete = albArray.shift()
+    if(album2.Name === albumToDelete){
+        album2.splice(0);
+    }else if(album3.Name === albumToDelete){
+        album3.splice(0);
+    }else if(album4.Name === albumToDelete){
+        album4.splice(0);
+    }else if(album5.Name === albumToDelete){
+        album5.splice(0);
+    }else if(album6.Name === albumToDelete){
+        album6.splice(0);
+    }else if(album7.Name === albumToDelete){
+        album7.splice(0);
+    }else if(album8.Name === albumToDelete){
+        album8.splice(0);
+    }else if(album9.Name === albumToDelete){
+        album9.splice(0);
+    }else if(album10.Name === albumToDelete){
+        album10.splice(0);
+    }
+    var x = document.getElementById(albumToDelete);
+    x.style.display = "none";
+    x.classList.remove("selectedAlbum");
+    setAlbum(album1, "album1");
+}
+function delete_album(){
+    if(albArray[0] === "album1"){
+        alert("Album 1 can not be delete!");
+        return;
+    }else{
+        hide_album();
+    }
+}
+
+function initialize(){
+    setAlbum(album1, "album1");
+    show_num();
+    var x = document.getElementById("album5");
+    x.style.display = "none";
+    x = document.getElementById("album6");
+    x.style.display = "none";
+    x = document.getElementById("album7");
+    x.style.display = "none";
+    x = document.getElementById("album8");
+    x.style.display = "none";
+    x = document.getElementById("album9");
+    x.style.display = "none";
+    x = document.getElementById("album10");
+    x.style.display = "none";
+}
+
+function show_num(){
+    let picNum = document.getElementById("picNum");
+    
+    let target = picArray[0];
+    let numInstr = target.slice(10);
+    let numInInt = parseInt(numInstr);
+    numInInt++;
+    numInstr = numInInt.toString();
+    let txt = numInstr + "/";
+    if(album1.Name === albArray[0]){
+        txt += album1.length;
+    }else if(album2.Name === albArray[0]){
+        txt += album2.length;
+    }else if(album3.Name === albArray[0]){
+        txt += album3.length;
+    }else if(album4.Name === albArray[0]){
+        txt += album4.length;
+    }else if(album5.Name === albArray[0]){
+        txt += album5.length;
+    }else if(album6.Name === albArray[0]){
+        txt += album6.length;
+    }else if(album7.Name === albArray[0]){
+        txt += album7.length;
+    }else if(album8.Name === albArray[0]){
+        txt += album8.length;
+    }else if(album9.Name === albArray[0]){
+        txt += album9.length;
+    }else if(album10.Name === albArray[0]){
+        txt += album10.length;
+    }
+
+    picNum.textContent = txt;
+}
+
+initialize();
