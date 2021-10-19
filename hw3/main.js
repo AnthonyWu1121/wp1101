e87ca99b1beec7651fa47ea76a1d49c8a85e4275
unknown <anthony.hc.wu@gmail.com>
2021-10-19 21:56:41 +0800

    2021/10/19 21:56

--- hw3/main.js
+++ hw3/main.js
@@ -1,18 +1,21 @@
 let itemCount = 0;
-let itemList = [];
+// let itemList = [];
+
+let idCount = 0;
 
 let todoCount = 0;
 let completedCount = 0;
 let statusList = [];
+const Status_Todo = 0, Status_Completed = 1, Status_Deleted = -1;
 
 function listCounting(){
-    itemCount = itemList.length;
+    // itemCount = itemList.length;
     todoCount = 0;
     completedCount = 0;
     for(let i = 0; i < statusList.length; i++){
-        if(statusList[i] === 0){
+        if(statusList[i] === Status_Todo){
             todoCount++;
-        }else{
+        }else if(statusList[i] === Status_Completed){
             completedCount++;
         }
     }
@@ -33,7 +36,9 @@ main.appendChild(input_bar);
 let todo_list = document.createElement("ul");
 todo_list.id = "todo-list";
 todo_list.classList.add("todo-app__list");
+todo_list.style.overflow = "auto";
 main.appendChild(todo_list);
+todo_list.style.display = "none";
 
 let footer = document.createElement("footer");
 footer.id = "todo-footer";
@@ -44,10 +49,13 @@ let view = document.createElement("ul");
 view.classList.add("todo-app__view-buttons");
 let all_button = document.createElement("button");
 all_button.textContent = "All";
+all_button.onclick = function(){showAll();};
 let active_button = document.createElement("button");
 active_button.textContent = "Active";
+active_button.onclick = function(){showTodo();};
 let completed_button = document.createElement("button");
 completed_button.textContent = "Completed";
+completed_button.onclick = function(){showCompleted();};
 view.appendChild(all_button);
 view.appendChild(active_button);
 view.appendChild(completed_button);
@@ -55,12 +63,14 @@ let clean = document.createElement("div");
 clean.classList.add("todo-app__clean");
 let clear_button = document.createElement("button");
 clear_button.textContent = "Clear completed";
+clear_button.onclick = function(){claerCompleted();};
 clean.appendChild(clear_button);
 footer.appendChild(total);
 footer.appendChild(view);
 footer.appendChild(clean);
 root.appendChild(footer);
 footer.style.display = "none";
+clear_button.style.display = "none";
 
 input_bar.addEventListener('keypress', function (e) {
     let new_todo = input_bar.value;
@@ -70,25 +80,32 @@ input_bar.addEventListener('keypress', function (e) {
         input_bar.value = "";
         itemCount++;
         todoCount++;
-        itemList.push(new_todo);
-        statusList.push(0);
+        idCount++;
+        // itemList.push(new_todo);
+        statusList.push(Status_Todo);
         showFooter();
     }
 });
 
 function showFooter(){
     listCounting();
-    if(todoCount !== 0){
+    if(itemCount !== 0){
+        todo_list.style.display = "";
         footer.style.display = "";
         total.textContent = "" + todoCount + " left";
-    }else if(todoCount === 0){
+    }else if(itemCount === 0){
+        todo_list.style.display = "none";
         footer.style.display = "none";
-        total.textContent = "" + todoCount + " left";
+    }
+    if(completedCount !== 0){
+        clear_button.style.display = "";
+    }else{
+        clear_button.style.display = "none";
     }
 }
 
 function addItem(new_todo){
-    let num = itemCount;
+    let num = idCount;
     let item = document.createElement("li");
     item.classList.add("todo-app__item");
     let check_box = document.createElement("div");
@@ -107,6 +124,7 @@ function addItem(new_todo){
     let x = document.createElement("img");
     x.src = "img/x.png";
     x.classList.add("todo-app__item-x");
+    x.onclick = function(){removeItem(num, this.parentElement);};
     item.appendChild(check_box);
     item.appendChild(detail);
     item.appendChild(x);
@@ -118,11 +136,68 @@ function doneTodo(n, detail){
     
     if(input.checked === true){
         detail.style="text-decoration: line-through; opacity: 0.5;";
-        statusList[n] = 1;
+        statusList[n] = Status_Completed;
     }else if(input.checked === false){
         detail.style="text-decoration: none; opacity: 1;";
-        statusList[n] = 0;
+        statusList[n] = Status_Todo;
     }
     showFooter();
 }
 
+function removeItem(n, item){
+    statusList[n] = Status_Deleted;
+    // itemList.splice(n, 1);
+    itemCount--;
+    item.remove();
+    showFooter();
+}
+
+function showAll(){
+    let len = statusList.length;
+    for(let i = 0; i < len; i++){
+        if(statusList[i] === Status_Todo || statusList[i] === Status_Completed){
+            let completedItem = document.getElementById(i).parentElement.parentElement;
+            completedItem.style.display = "";
+        }
+    }
+}
+
+function showTodo(){
+    let len = statusList.length;
+    for(let i = 0; i < len; i++){
+        if(statusList[i] === Status_Completed){
+            let completedItem = document.getElementById(i).parentElement.parentElement;
+            completedItem.style.display = "none";
+        }else if(statusList[i] === Status_Todo){
+            let completedItem = document.getElementById(i).parentElement.parentElement;
+            completedItem.style.display = "";
+        }
+    }
+}
+
+function showCompleted(){
+    let len = statusList.length;
+    for(let i = 0; i < len; i++){
+        if(statusList[i] === Status_Todo){
+            let completedItem = document.getElementById(i).parentElement.parentElement;
+            completedItem.style.display = "none";
+        }else if(statusList[i] === Status_Completed){
+            let completedItem = document.getElementById(i).parentElement.parentElement;
+            completedItem.style.display = "";
+        }
+    }
+}
+
+function claerCompleted(){
+    let len = statusList.length;
+    for(let i = 0; i < len; i++){
+        if(statusList[i] === Status_Completed){
+            let completedItem = document.getElementById(i).parentElement.parentElement;
+            completedItem.remove();
+            statusList[i] = Status_Deleted;
+            // itemList.splice(n, 1);
+            itemCount--;
+        }
+    }
+    showFooter();
+}
